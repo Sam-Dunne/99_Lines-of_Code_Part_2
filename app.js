b23352b2f31c9e427134b063b27d702cca9f7d83
Sam-Dunne <cras.sdunne@gmail.com>
2021-01-31 11:03:58 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+
+let friends = ['Chase', 'Trey', 'Katie', 'Devonte', 'Emil'];
+let singButton = document.querySelector('button');
+
+
+singButton.addEventListener('click', function () {
+
+    friends.forEach(friend => {
+
+        let friendsDiv = document.createElement('div');
+        document.body.appendChild(friendsDiv);
+        friendsDiv.className = 'friend';
+
+        let friendHeader = document.createElement('h3');
+        friendsDiv.appendChild(friendHeader);
+        let friendNameText = document.createTextNode(friend);
+        friendHeader.appendChild(friendNameText);
+
+        let songLinesParagraph = document.createElement('p');
+        friendsDiv.appendChild(songLinesParagraph);
+
+        for (let num = 99; num >= 1; num--) {
+            if (num === 2) { 
+                let songLinesText = document.createTextNode
+                    (num + ' lines of code in the file, ' + num + ' lines of code; ' + friend + ' strikes one out, clears it all out, ' + (num - 1) + ' line of code in the file, ');
+                songLinesParagraph.appendChild(songLinesText);
+            } else if (num === 1) {
+                let songLinesText = document.createTextNode
+                    (num + ' line of code in the file, ' + num + ' line of code; ' + friend + ' strikes one out, clears it all out, ' + (num - 1) + ' lines of code in the file.');
+                songLinesParagraph.appendChild(songLinesText);
+            } else if (num > 2) {
+                let songLinesText = document.createTextNode(num + ' lines of code in the file, ' + num + ' lines of code; ' + friend + ' strikes one out, clears it all out ' + (num - 1) + ' lines of code in the file, ');
+                songLinesParagraph.appendChild(songLinesText);
+            };
+        };
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
