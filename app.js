b702b338da889e2d6109d1896247ec0170bbf7c7
Sam-Dunne <cras.sdunne@gmail.com>
2021-02-01 08:38:17 -0600

    complete with comments

--- app.js
+++ app.js
@@ -6,19 +6,19 @@ let singButton = document.querySelector('button');
 singButton.addEventListener('click', function () {
 
     friends.forEach(friend => {
-
+//creates, appends, modifies class for div
         let friendsDiv = document.createElement('div');
         document.body.appendChild(friendsDiv);
         friendsDiv.className = 'friend';
-
+//creates h3 element, appends inside div, creates text node for header
         let friendHeader = document.createElement('h3');
         friendsDiv.appendChild(friendHeader);
         let friendNameText = document.createTextNode(friend);
         friendHeader.appendChild(friendNameText);
-
+//creates paragraph element, appends in div
         let songLinesParagraph = document.createElement('p');
         friendsDiv.appendChild(songLinesParagraph);
-
+//Loops thru song lyric logic. creates and appends para textNode for each condition 
         for (let num = 99; num >= 1; num--) {
             if (num === 2) { 
                 let songLinesText = document.createTextNode
